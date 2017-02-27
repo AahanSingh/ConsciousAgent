42a0e7df45f4aac4561c7aed162626511f29d437
Aahan Singh <aahansingh95@gmail.com>
2017-02-28 00:13:21 +0530

    fixed script

--- lstm_experiment.py
+++ lstm_experiment.py
@@ -1,67 +1,41 @@
-# Load Larger LSTM network and generate text
-import sys
-import numpy
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import LSTM
+from keras.layers import LSTM, Dense, Dropout
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
-# load ascii text and covert to lowercase
-filename = "wonderland.txt"
-raw_text = open(filename).read()
-raw_text = raw_text.lower()
-# create mapping of unique chars to integers, and a reverse mapping
-chars = sorted(list(set(raw_text)))
-char_to_int = dict((c, i) for i, c in enumerate(chars))
-int_to_char = dict((i, c) for i, c in enumerate(chars))
-# summarize the loaded data
-n_chars = len(raw_text)
-n_vocab = len(chars)
-print "Total Characters: ", n_chars
-print "Total Vocab: ", n_vocab
-# prepare the dataset of input to output pairs encoded as integers
+import numpy as np
+
+file = 'alice.txt'
+text = open(file).read()
+text = text.lower()
+chars = sorted(list(set(text)))
+char_to_int = dict((c,i) for i,c in enumerate(chars))
+n_chars = len(text) #total no of chars
+n_vocab = len(chars)#no of unique chars
 seq_length = 100
 dataX = []
 dataY = []
-for i in range(0, n_chars - seq_length, 1):
-	seq_in = raw_text[i:i + seq_length]
-	seq_out = raw_text[i + seq_length]
-	dataX.append([char_to_int[char] for char in seq_in])
-	dataY.append(char_to_int[seq_out])
-n_patterns = len(dataX)
-print "Total Patterns: ", n_patterns
-# reshape X to be [samples, time steps, features]
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-# normalize
-X = X / float(n_vocab)
-# one hot encode the output variable
+for i in range(0,n_chars-seq_length,1):
+    seq_in = text[i:i+seq_length]
+    seq_out = text[i+seq_length]
+    dataX.append([char_to_int[char] for char in seq_in])
+    dataY.append(char_to_int[seq_out])
+n_patt = len(dataX)
+# RESHAPE TO GIVE INPUT TO LSTM :- (SAMPLES, TIMESTEPS OR NUMBER OF CHARS, FEATURES)
+X = np.reshape(dataX,(n_patt,seq_length,1))
+# NORMALIZE 0->1
+X = X/float(n_vocab)
+# ONE HOT ENCODE OUTPUT VECTOR
 y = np_utils.to_categorical(dataY)
-# define the LSTM model
+# LSTM WITH 256 MEM UNITS
+print X.shape
+print y.shape
 model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(LSTM(256,input_dim=1, input_length=100))
 model.add(Dropout(0.2))
-model.add(LSTM(256))
-model.add(Dropout(0.2))
-model.add(Dense(y.shape[1], activation='softmax'))
-# load the network weights
-filename = "weights-improvement-47-1.2219-bigger.hdf5"
-model.load_weights(filename)
+model.add(Dense(y.shape[1],activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
-# pick a random seed
-start = numpy.random.randint(0, len(dataX)-1)
-pattern = dataX[start]
-print "Seed:"
-print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
-# generate characters
-for i in range(1000):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
-	x = x / float(n_vocab)
-	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
-	result = int_to_char[index]
-	seq_in = [int_to_char[value] for value in pattern]
-	sys.stdout.write(result)
-	pattern.append(index)
-	pattern = pattern[1:len(pattern)]
-print "\nDone."
+
+file = 'weights-improvement-{epoch:02d}-{loss:.4f}.hdf5'
+checkpoint = ModelCheckpoint(file,monitor='loss',verbose=1,save_best_only=True,mode='min')
+callbacks_list = [checkpoint]
+model.fit(X,y,nb_epoch=20,batch_size=256,callbacks=callbacks_list)
