624027fe941e494d11d733339379a954c94a25bf
aahansingh <aahansingh95@gmail.com>
2017-04-21 04:57:49 -0700

    Completed Beam Search

--- CaffeModel/generate_captions.py
+++ CaffeModel/generate_captions.py
@@ -4,23 +4,27 @@ import random
 import numpy as np
 from create_dataset_to_h5 import get_vocab
 from PIL import Image
+import time
+import os
 
-def predict_single_word(image,previous_word,output='probs'):
+def predict_single_word(previous_word,output='probs'):#def predict_single_word(image,previous_word,output='probs'):
     image = img
     clip = 0 if previous_word==1 else 1
     word = np.array([previous_word])
     clip = np.array([clip])
     net.forward(data=image,clip=clip,input_sentence=word)
     preds = net.blobs[output].data[0,0,:]
+    #print preds.shape
     return preds
 
-def predict_word_based_on_all_previous_words(previous_words,net,image):
+def predict_word_from_all_previous_words(previous_words):#def predict_word_from_all_previous_words(previous_words,net,image):
     preds=0
     for word in previous_words:
-        preds=predict_single_word(net,word,image)
+        #print word
+        #print 'IN LOOP'
+        preds=predict_single_word(word)
+    #print "IN ALL PREV"
     return preds
-
-
 
 def predict_caption(descriptor, strategy={'type': 'temp'}):
 
@@ -56,7 +60,7 @@ def random_choice_from_probs(softmax_inputs, temp=1, already_softmaxed=False):
     return np.argmax(softmax_inputs)
   if already_softmaxed:
     probs = softmax_inputs
-    assert temp == 1
+   # assert temp == 1
   else:
     probs = softmax(softmax_inputs, temp)
   r = random.random()
@@ -72,120 +76,101 @@ def softmax(softmax_inputs, temp):
   exp_outputs_sum = exp_outputs.sum()
   if math.isnan(exp_outputs_sum):
     return exp_outputs * float('nan')
-  assert exp_outputs_sum > 0
+  #assert exp_outputs_sum > 0
   if math.isinf(exp_outputs_sum):
     return np.zeros_like(exp_outputs)
   eps_sum = 1e-20
   return exp_outputs / max(exp_outputs_sum, eps_sum)
 
-'''def predict_caption_beam_search(descriptor, strategy, max_length=50):
-    orig_batch_size = set_caption_batch_size()
-    if orig_batch_size != 1: set_caption_batch_size(1)
-    beam_size = strategy['beam_size'] if 'beam_size' in strategy else 1
-    assert beam_size >= 1
-    beams = [[]]
-    beams_complete = 0
-    beam_probs = [[]]
-    beam_log_probs = [0.]
-    while beams_complete < len(beams):
-      expansions = []
-      for beam_index, beam_log_prob, beam in \
-          zip(range(len(beams)), beam_log_probs, beams):
-        if beam:
-          previous_word = beam[-1]
-          if len(beam) >= max_length or previous_word == 0:
-            exp = {'prefix_beam_index': beam_index, 'extension': [],
-                   'prob_extension': [], 'log_prob': beam_log_prob}
-            expansions.append(exp)
-            # Don't expand this beam; it was already ended with an EOS,
-            # or is the max length.
-            continue
-        else:
-          previous_word = 0  # EOS is first word
-        if beam_size == 1:
-          probs = predict_single_word(descriptor, previous_word)
-        else:
-          probs = predict_single_word_from_all_previous(descriptor, beam)
-        assert len(probs.shape) == 1
-        assert probs.shape[0] == len(vocab)
-        expansion_inds = probs.argsort()[-beam_size:]
-        for ind in expansion_inds:
-          prob = probs[ind]
-          extended_beam_log_prob = beam_log_prob + math.log(prob)
-          exp = {'prefix_beam_index': beam_index, 'extension': [ind],
-                 'prob_extension': [prob], 'log_prob': extended_beam_log_prob}
-          expansions.append(exp)
-      # Sort expansions in decreasing order of probability.
-      expansions.sort(key=lambda expansion: -1 * expansion['log_prob'])
-      expansions = expansions[:beam_size]
-      new_beams = \
-          [beams[e['prefix_beam_index']] + e['extension'] for e in expansions]
-      new_beam_probs = \
-          [beam_probs[e['prefix_beam_index']] + e['prob_extension'] for e in expansions]
-      beam_log_probs = [e['log_prob'] for e in expansions]
-      beams_complete = 0
-      for beam in new_beams:
-        if beam[-1] == 0 or len(beam) >= max_length: beams_complete += 1
-      beams, beam_probs = new_beams, new_beam_probs
-    if orig_batch_size != 1: set_caption_batch_size(orig_batch_size)
-    return beams, beam_probs
-'''
-def set_caption_batch_size(batch_size,lstm_net):
-    lstm_net.blobs['cont_sentence'].reshape(1, batch_size)
-    lstm_net.blobs['input_sentence'].reshape(1, batch_size)
-    lstm_net.blobs['image_features'].reshape(batch_size, lstm_net.blobs['image_features'].data.shape[1:])
-    lstm_net.reshape()
-
-def gen_caption(vocab):
-    '''
-    THIS FUNCTION WILL GENERATE CAPTIONS.
-    IT WILL USE THE TOP 5 MOST LIKELY WORDS AND RUN A* TO FIND THE BEST CAPTION.
-    :param vocab:
-    :return:
-    '''
-
-    vocab = get_vocab()
-    vinv = {}
-    # OBTAIN DICTIONARY
-    for i, w in enumerate(vocab):
-        vinv[vocab[w]] = w
-    # OBTAIN IMAGE
-    img_file = '/home/aahansingh/ConsciousAgent/CaffeModel/Flicker8k_Dataset/2436081047_bca044c1d3.jpg'
-    img = Image.open(img_file)
-    img = img.resize((224, 224), Image.NEAREST)
-    img = np.array(img)
-    img = np.rollaxis(img, 2)
-    img = img[np.newaxis, :, :, :]
-    net = caffe.Net('LRCN.deploy.prototxt', 'LRCNModel_iter_50000.caffemodel', caffe.TEST)
-    net.blobs['data'].reshape(*img.shape)
-
-    word = 1 # 1==<START>
-    preds = predict_single_word(net,word,img)
-    top_5_words = np.argsort(-preds) # HOLDS THE INDICES OF THE TOP 5 WORDS
-    top_5_words = top_5_words[:5]
-    top_5_preds=np.zeros(5) # HOLDS THE PROBABILITIES OF TOP 5 WORDS
-    for i,j in enumerate(top_5_words):
-        top_5_preds[i]=preds[j]
+def beam_search(beam_size, max_len = 40):
+	beams = [[]]
+	beams_complete = 0
+	beam_probs = [[]]
+	word_probs = [[]]
+	complete_beams=[]
+	complete_beams_probs = []
+	while beams_complete<len(beams):
+		print "*******************************COMPLETE BEAMS = %d***************************" % beams_complete
+		child_nodes = [] # HOLDS EXPANDED NODES
+		for index,beam, beam_prob, word_prob in zip(range(len(beams)),beams,beam_probs,word_probs):
+			print '############################## BEAM %d #######################' % index
+			print beam
+			# FINDING PREVIOUS WORD
+			if beam: # IF BEAM EXIXTS
+				prev_word = beam[-1]
+				if prev_word == 0 or len(beam)>=max_len: # <END> == 0
+					#node = {'parent_beam_index': index, 'word': [], 'prob_word': word_prob, 'prob_beam': beam_prob}
+					#child_nodes.append(node)
+					continue
+			else:
+				prev_word = 1 # <START> SYMBOL
+				beam.append(prev_word)
+			# OBTAIN PREDICTIONS
+			if beam_size==1:
+				preds = predict_single_word(prev_word)
+			else:
+				preds = predict_word_from_all_previous_words(beam)
+			top_n_indices = np.argsort(-preds)[:beam_size]
+			top_n_probs = preds[top_n_indices] # HOLDS CUMULATIVE PROB OF ENTIRE BEAM
+			# FINDING CUMULATIVE PROB OF ENTIRE BEAM
+			for i,p in enumerate(top_n_probs):
+				if beam[-1]!=1:
+					top_n_probs[i] = beam_prob[-1]+p 
+			# CREATING NODES
+			print "######################### EXPANSION INDS ############################"
+			print top_n_indices
+			for i,j in enumerate(top_n_indices):
+				prob_b = top_n_probs[i]
+				prob_w = preds[j]
+				node = {'parent_beam_index': index, 'word': [j], 'prob_word': [prob_w], 'prob_beam':[prob_b]}
+				child_nodes.append(node)
+			print "######################### CHILD NODES IN LOOP ############################"
+			print child_nodes    
+		'''# SORTING CHILD NODES BY WORD PROB
+		child_nodes.sort(key=lambda x:x['prob_word'],reverse=True)
+		# TAKE ONLY TOP N CHILD NODES
+		child_nodes = child_nodes[:beam_size]
+		# SORTING CHILD NODES BY BEAM PROB'''
+		child_nodes.sort(key=lambda x:x['prob_beam'],reverse=True)
+		# TAKE ONLY TOP N CHILD NODES
+		child_nodes = child_nodes[:beam_size]
+		print "######################### SORTED CHILD NODES ############################"
+		print child_nodes
+		new_beams = [beams[c['parent_beam_index']] + c['word'] for c in child_nodes]
+		new_beam_probs = [beam_probs[c['parent_beam_index']] + c['prob_beam'] for c in child_nodes]
+		new_word_probs = [word_probs[c['parent_beam_index']] + c['prob_word'] for c in child_nodes]
+		for i,beam in enumerate(new_beams):
+			if beam[-1] == 0 or len(beam)>max_len:
+				beams_complete+=1
+				complete_beams.append(beam)
+				complete_beams_probs.append(new_beam_probs[i][-1])
+		beams,beam_probs, word_probs = new_beams,new_beam_probs, new_word_probs
+		'''if len(new_beams) > beam_size:
+			beams,beam_probs, word_probs = new_beams,new_beam_probs, new_word_probs
+		else:
+			beams, beam_probs, word_probs = (new_beams+beams)[:beam_size], (new_beam_probs+beam_probs)[:beam_size], (new_word_probs+word_probs)[:beam_size]
+		'''
+		print "######################### NEW BEAMS ############################"
+		print beams
+	return beams,beam_probs
+	complete_beams_probs = np.array(complete_beams_probs)
+	ind = np.argsort(-complete_beams_probs)
+	print ind
+	print complete_beams
+	print complete_beams_probs
+	copy = []
+	for i in ind:
+		copy.append(complete_beams[i])
+	complete_beams_probs = complete_beams_probs[ind]
+	return copy, complete_beams_probs
 
 
 
-    for i in range(40):
-        print "INPUT WORD = %s" % vinv[word]
-        if i == 1:
-            first = predict_single_word(net, 1, img)
-        else:
-            first = predict_single_word(net, word, img)
-        first = np.argsort(-first)  # ARGSORT SORTS IN ASCENDING. HERE WE SORT IN DESCENDING
-        print first[:10]
-        print_word(first, vinv)
-        print '\n'
-        word = first[0]
-
-
-def print_word(x,vocab):
+def print_sentence(x):
+    s = ""
     for i in x:
-        print vocab[i]
-
+        s+=" "+vinv[i]
+    print s
 def process_img(img_file):
     img = Image.open(img_file)
     img = img.resize((224, 224), Image.NEAREST)
@@ -195,41 +180,53 @@ def process_img(img_file):
     return img
 
 def gen_cap():
-	word = 1	
-	cum_sum=0.
-	preds = 0
-	sent = ""	
-	for _ in range(100):
-		if word==0:
-			break
-		print vinv[word]			
-		#word = raw_input("ENTER INDEX")
-		#word = int(word)
-		if word!=1:
-			cum_sum+= preds[word]		
-		preds = predict_single_word(img,word,output='probs')
-		top_10 = np.argsort(-preds)[:10] # HOLDS THE TOP 10 INDICES
-		#print top_10		
-		word = top_10[0]				
-		#print_word(top_10,vinv)
-		sent+=" "+vinv[word]
-	print cum_sum
-import os
+    word = 1
+    cum_sum=0.
+    preds = 0
+    sent = ""
+    for _ in range(100):
+        if word==0:
+            break
+        print vinv[word]
+        word = raw_input("ENTER INDEX")
+        word = int(word)
+        if word!=1:
+            cum_sum+= preds[word]
+        preds = predict_single_word(word)
+        top_10 = np.argsort(-preds)[:10] # HOLDS THE TOP 10 INDICES
+        print top_10
+        #word = top_10[0]
+        print_word(top_10,vinv)
+        sent+=" "+vinv[word]
+    print sent
+    print cum_sum
+
 vocab = get_vocab()
 vinv = {}
 # OBTAIN DICTIONARY
 for i, w in enumerate(vocab):
     vinv[vocab[w]] = w
 # OBTAIN IMAGE
-img_file = os.getcwd()+'/girl.jpeg'
+img_file = os.getcwd()+'/man.jpg'
 img = process_img(img_file)
 
-net = caffe.Net('LRCN.deploy.prototxt', 'LRCNModel_iter_60000.caffemodel', caffe.TEST)
+net = caffe.Net('LRCN.deploy.prototxt', 'LRCN_Finetune_Model_iter_30000.caffemodel', caffe.TEST)
 net.blobs['data'].reshape(*img.shape)
 
-gen_cap()
-'''caption,probs = predict_caption(1223,'not_')
-print caption
-print vinv[1]
-print_word(caption[0],vinv)
-'''
+#gen_cap()
+start_time = time.time()
+beam_size = int(raw_input("ENTER BEAM SIZE: "))
+beams,probs = beam_search(beam_size)
+#print beams.shape
+#print probs.shape
+sent = ""
+'''for i in beams:
+    for j in i:
+        print vinv[j]
+for i in range(beam_size):
+	print probs[i][-1]'''
+for sent in beams:
+	print_sentence(sent)
+#print probs[0][-1]
+print time.time()-start_time
+os.system("play -n synth 2 sine 800 vol 0.5")
